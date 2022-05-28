220f4f459494c4d586072a2363128b83248c149f
Haoyu Zhuang <zhuanghaoyurichard@gmail.com>
2022-05-28 11:12:51 -0400

    Downloading surface data

--- era5.py
+++ era5.py
@@ -41,6 +41,47 @@ HOUR_INTERVAL = [
 ]
 NATIVE_GRID = [0.25, 0.25]
 ONE_DEG_GRID = [1.0, 1.0]
+SURFACE_VARIABLES = [
+    '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
+    '2m_temperature', 'mean_sea_level_pressure', 'precipitation_type',
+    'total_column_water', 'total_column_water_vapour', 'total_precipitation',
+    'vertical_integral_of_eastward_water_vapour_flux', 'vertical_integral_of_northward_water_vapour_flux',
+]
+
+
+def surface_data(folder, variables=SURFACE_VARIABLES, days=DAYS, time=THREE_HOUR_INTERVAL,
+                 area=US_SUBSET, grid=NATIVE_GRID, years=[1979, 2022]):
+    """
+    Download ERA-5 Surface level data. 
+    """
+    if len(years) != 2:
+        assert "The parameters years have to be a list of size 2."
+    if not os.path.isdir(folder):
+        os.makedirs(folder)
+    for year in range(*years):
+        yr_folder = os.path.join(folder, str(year))
+        if not os.path.isdir(yr_folder):
+            os.makedirs(yr_folder)
+        for month in [11, 12, 1, 2, 3]:
+            yr = year if month in [11, 12] else year + 1
+            fn = f'e5_{yr}{month:02d}_sfc.nc'
+            fp = os.path.join(yr_folder, fn)
+            print('=========================================================')
+            print(f'Downloading {yr}-{month:02d}')
+            c.retrieve(
+                'reanalysis-era5-single-levels',
+                {
+                    'product_type': 'reanalysis',
+                    'format': 'netcdf',
+                    'variable': variables,
+                    'year': f'{yr}',
+                    'month': f'{month:02d}',
+                    'day': days,
+                    'time': time,
+                    'area': area,
+                    'grid': grid
+                },
+                fp)
 
 
 def pressure_data(folder, variable, days=DAYS, time=THREE_HOUR_INTERVAL,
@@ -49,6 +90,8 @@ def pressure_data(folder, variable, days=DAYS, time=THREE_HOUR_INTERVAL,
     """
     Download ERA-5 Pressure level data. 
     """
+    if len(years) != 2:
+        assert "The parameters years have to be a list of size 2."
     if not os.path.isdir(folder):
         os.makedirs(folder)
     for year in range(*years):
@@ -79,4 +122,5 @@ def pressure_data(folder, variable, days=DAYS, time=THREE_HOUR_INTERVAL,
 
 
 if __name__ == '__main__':
-    pressure_data('reanalysis', 'temperature', years=[1979, 1994])
+    surface_data('reanalysis', years=[2012, 2013])
+    # pressure_data('reanalysis', 'temperature', years=[1979, 1994])
